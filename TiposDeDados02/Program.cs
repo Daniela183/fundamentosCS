097293c1cf551782f0ac39d830f76ccbe0470663
Daniela Soares <danielasmelo232@gmail.com>
2024-05-28 21:15:01 -0300

    fundamentos

---
+++ TiposDeDados02/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Atribuíndo valores double, float e decimal");
+Console.WriteLine();
+
+double n1 = 1.234;
+float n2 = 1.234f;
+decimal n3 = 1.234m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+
+Console.ReadLine();
+
+Console.WriteLine("Coparando a precição de double, float e decimal");
+Console.WriteLine();
+
+double y = 1d / 3d;
+float x = 1f / 3f;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadLine();
